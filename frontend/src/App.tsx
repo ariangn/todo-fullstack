c7d01610e48cd6a21f9cbac5940c135c4b816316
ariangn <arianakamura@gmail.com>
2025-06-02 01:31:09 -0500

    vite + tailwind scaffolding

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+// src/App.tsx
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind test
+      </h1>
+    </div>
+  );
+}
